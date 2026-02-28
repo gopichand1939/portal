de590d83f7fd6a8a35c1c65d80fb11ebed968e72
gopichand <tummapalagopichand@gmail.com>
2026-02-28 09:19:20 +0530

    Initial commit

--- Frontend/lib/constants.ts
+++ Frontend/lib/constants.ts
@@ -1,6 +1,6 @@
-// const DEPLOYED_BACKEND = 'https://learning-portal-e19t.onrender.com'
+const DEPLOYED_BACKEND = 'https://learning-portal-e19t.onrender.com'
 
-const DEPLOYED_BACKEND = 'http://localhost:15013'
+//const DEPLOYED_BACKEND = 'http://localhost:15013'
 export const API_BASE_URL = DEPLOYED_BACKEND
 export const API_AUTH_LOGIN = DEPLOYED_BACKEND + '/api/auth/login'
 export const API_AUTH_REGISTER = DEPLOYED_BACKEND + '/api/auth/register'
